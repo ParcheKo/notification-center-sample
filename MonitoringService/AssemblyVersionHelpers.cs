a6bb4d184aac6abb2e4e22160638089d5b3d15bf
Rezaei, AmirAbbas (GIG) <Rezaei.AmirAbbas@Golrang.com>
2021-11-22 00:13:09 +0330

    Fix

--- MonitoringService/AssemblyVersionHelpers.cs
+++ MonitoringService/AssemblyVersionHelpers.cs
@@ -7,12 +7,12 @@ namespace MonitoringService
     {
         public static string GetAssemblyVersion(this string filePath)
         {
-            return FileVersionInfo.GetVersionInfo(filePath).ProductVersion;
+            return FileVersionInfo.GetVersionInfo(filePath).ProductVersion!;
         }
 
         public static string GetAppVersion()
         {
-            return FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location).ProductVersion;
+            return FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly()!.Location).ProductVersion!;
         }
     }
 }
