49f85d597565e8e653c9b84af7f11bd95179dc49
Rezaei, AmirAbbas (GIG) <Rezaei.AmirAbbas@Golrang.com>
2021-12-02 17:57:02 +0330

    Organization

---
+++ MonitoringService/Commands/Deploy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace MonitoringService.Commands
+{
+    public class Deploy : IRequest
+    {
+        public class Deployer : IRequestHandler<Deploy>
+        {
+            public async Task<Unit> Handle(
+                Deploy request,
+                CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
